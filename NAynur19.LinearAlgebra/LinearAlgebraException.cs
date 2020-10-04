378ef15770c3ef1e4834fdc1f9e0b8a6c57e25d7
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2020-10-05 00:26:18 +0300

    Added: the new vector exception messages

--- NAynur19.LinearAlgebra/LinearAlgebraException.cs
+++ NAynur19.LinearAlgebra/LinearAlgebraException.cs
@@ -2,21 +2,48 @@
 
 namespace NAynur19.LinearAlgebra
 {
+	/// <summary>
+	/// Класс исключений, наследуемый от базового класса исключений.
+	/// </summary>
 	public class LinearAlgebraException : Exception
 	{
+		/// <summary>
+		/// Конструктор, наследующий базовую логику.
+		/// </summary>
 		public LinearAlgebraException() : base() { }
+
+		/// <summary>
+		/// Конструктор, наследующий базовую логику.
+		/// </summary>
+		/// <param name="message">Сообщение об ошибке.</param>
 		public LinearAlgebraException(string message) : base(message) { }
+
+		/// <summary>
+		/// Конструктор, наследующий базовую логику.
+		/// </summary>
+		/// <param name="message">Сообщение об ошибке</param>
+		/// <param name="innerException">Внутреннее исключение.</param>
 		public LinearAlgebraException(string message, Exception innerException) : base(message, innerException) { }
 	}
 
+	/// <summary>
+	/// Класс сообщений исключений.
+	/// </summary>
 	public static class LinearAlgebraExceptionMessage
 	{
-		public static string MatrixSizeLessOneException => "Размер матрицы не может быть меньше 1";
-		public static string VectorSizeLessOneException => "Размер вектора не может быть меньше 1";
-		public static string IndexOutOfMatrixSizeException => "Индекс находится за пределами массива.";
+		#region Vector Exceptions
+		public static string VectorSizeLessOneException => "Размер вектора не может быть меньше 1.";
 		public static string IndexOutOfVectorSizeException => "Индекс находится за пределами массива.";
+		public static string StartIndexOutOfVectorSizeException => "Начальный индекс находится за пределами массива.";
+		public static string EndIndexOutOfVectorSizeException => "Конечный индекс находится за пределами массива.";
+		public static string EndIndexLessStartIndexOfVectorException => "Конечный индекс меньше стартового индекса массива.";
+		public static string VectorSizeNonEqualException => "Размеры векторов не одинаковы.";
+
+		#endregion
+
+		public static string MatrixSizeLessOneException => "Размер матрицы не может быть меньше 1";
+		public static string IndexOutOfMatrixSizeException => "Индекс находится за пределами массива.";
 		public static string MatrixSizeNonEqual => "Размеры матриц не одинаковы";
-		public static string VectorSizeNonEqual => "Размеры векторов не одинаковы";
 		public static string MatrixSizeNonValidForMultiplication => "Размеры матриц не подходят для произведения";
 	}
 }
